bcb8ed7a667a290034a450ce0fd470d14d63d766
George Williams <george412williams@gmail.com>
2020-07-31 16:56:51 -0500

    plugging away

--- src/main/java/com/codeup/springblog/DiceController.java
+++ src/main/java/com/codeup/springblog/DiceController.java
@@ -3,6 +3,9 @@ package com.codeup.springblog;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import java.lang.Math;
 
 import java.util.ArrayList;
 
@@ -10,10 +13,17 @@ import java.util.ArrayList;
 public class DiceController {
 
     @GetMapping("/roll-dice")
-    //@ResponseBody
-    public String randomDieRoll(@PathVariable int num){
+    @ResponseBody
+    public String randomDieRoll(@RequestParam(name = "guess") int guess){
+            //(@PathVariable int num){
         ArrayList<Integer> dieRoll = new ArrayList<>();
+        int max = 6;
+        int min = 1;
+        int range = max - min + 1;
 
-        return "Random Die Roll Result: " + randomDieRoll(num);
+        for (int i = 0; i < 6; i++) {
+            int randDieRoll = (int)(Math.random() * range) + min;
+        }
+        return "Random Die Roll Result: ";
     }
 }
