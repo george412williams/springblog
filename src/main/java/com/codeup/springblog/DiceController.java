a88a9dbe0dc60061d9ce233c7f5805c078647b47
George Williams <george412williams@gmail.com>
2020-08-03 09:29:26 -0500

    added guess GET

--- src/main/java/com/codeup/springblog/DiceController.java
+++ src/main/java/com/codeup/springblog/DiceController.java
@@ -1,10 +1,11 @@
 package com.codeup.springblog;
 
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.thymeleaf.standard.expression.GreaterOrEqualToExpression;
+
 import java.lang.Math;
 
 import java.util.ArrayList;
@@ -12,18 +13,32 @@ import java.util.ArrayList;
 @Controller
 public class DiceController {
 
-    @GetMapping("/roll-dice")
-    @ResponseBody
-    public String randomDieRoll(@RequestParam(name = "guess") int guess){
-            //(@PathVariable int num){
-        ArrayList<Integer> dieRoll = new ArrayList<>();
-        int max = 6;
-        int min = 1;
-        int range = max - min + 1;
+//    @GetMapping("/roll-dice")
+//    public String randomDieRoll(){
+//            //(@PathVariable int num){
+//        ArrayList<Integer> dieRoll = new ArrayList<>();
+//        int max = 6;
+//        int min = 1;
+//        int range = max - min + 1;
+//
+//        for (int i = 0; i < 6; i++) {
+//            int randDieRoll = (int)(Math.random() * range) + min;
+//        }
+//        return "Random Die Roll Result: ";
+//    }
 
-        for (int i = 0; i < 6; i++) {
-            int randDieRoll = (int)(Math.random() * range) + min;
-        }
-        return "Random Die Roll Result: ";
+    @GetMapping("/roll-dice")
+    public String randDiceRoll(){
+        return "roll-dice";
     }
+
+    @RequestMapping(path = "/roll-dice/{guess}", method = RequestMethod.GET)
+        @ResponseBody
+        public String randDiceRoller(@PathVariable String guess, Model model) {
+            model.addAttribute("guess", guess);
+            return "roll-dice";
+    }
+
+
+
 }
