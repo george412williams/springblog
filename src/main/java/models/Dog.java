4a03fedb30df33727d8e6d392fc239dbc252afd0
George Williams <george412williams@gmail.com>
2020-08-03 16:45:00 -0500

    mini exercise adding dog and ad and dogdb

---
+++ src/main/java/models/Dog.java
@@ -0,0 +1,53 @@
+package models;
+
+
+import javax.persistence.*;
+
+@Entity
+//@Table(name = "dogs")
+public class Dog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private long age;
+    private String name;
+    private String resideState;
+
+    public Dog() {
+    }
+
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getAge() {
+        return age;
+    }
+
+    public void setAge(long age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getResideState() {
+        return resideState;
+    }
+
+    public void setResideState(String resideState) {
+        this.resideState = resideState;
+    }
+}
